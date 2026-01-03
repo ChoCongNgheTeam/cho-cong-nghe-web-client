8cfc17cbf5167b42d3bcad94cbff383c37e54d13
nguyenduc050514 <nguyenvanduc050514@gmail.com>
2026-01-03 12:26:46 +0700

    Merge: merge header/footer layout

--- app/(client)/layout.tsx
+++ app/(client)/layout.tsx
@@ -1,12 +1,16 @@
 import Header from "@/components/layout/Header/header";
 import Footer from "@/components/layout/Footer/footer";
 
-export default function ClientLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      {/* <Header /> */}
-      <main className="min-h-screen">{children}</main>
-      {/* <Footer /> */}
-    </>
-  );
+export default function ClientLayout({
+   children,
+}: {
+   children: React.ReactNode;
+}) {
+   return (
+      <>
+         <Header />
+         <main className="min-h-screen">{children}</main>
+         <Footer />
+      </>
+   );
 }
