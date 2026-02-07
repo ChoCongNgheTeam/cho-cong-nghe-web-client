1f92332224119ef6e3b40e0c40d3bd739914a7d8
Bao Tran <saichien862410@gmail.com>
2026-02-07 07:12:00 +0700

    Update profile

--- app/(client)/profile/page.tsx
+++ app/(client)/profile/page.tsx
@@ -1,69 +1,90 @@
 "use client";
-import { useState } from "react";
+import { useState, useEffect, use } from "react";
 import { Pencil } from "lucide-react";
+
+type Profile = {
+  userName: string;
+  email: string;
+  fullName: string;
+  phone: string;
+  gender: string;
+  role: string;
+  avatarImage: string;
+};
 
 export default function ProfilePage() {
   const [isEditing, setIsEditing] = useState(false);
+  const [profileData, setProfileData] = useState<Profile | null>(null);
 
-  const profile = {
-    name: "Bảo",
-    phone: "0828001501",
-    gender: "",
-  };
 
+
+  useEffect(() => {
+    const fecthProfile = async () => {
+      try {
+        const response = await fetch("http://localhost:5000/api/v1/users/me");
+        const data = await response.json();
+        setProfileData(data);
+      } catch (error) {
+        console.error("Error fetching profile data:", error);
+      }
+    };
+    fecthProfile();
+  }, []);
+
+  if (!profileData) return <div className="p-4">Đang tải...</div>;
   return (
-   <div>
-       {/* Header */}
+    <div>
+      {/* Header */}
       <h1 className="text-2xl font-semibold text-gray-800 mb-4 text-left mt-2">
         Thông tin cá nhân
       </h1>
-     <div className="px-4 py-10 bg-white">
-      {/* Header */}
-      <div className="w-full max-w-md mx-auto">
-        {/* Card */}
-        <div className=" overflow-hidden">
-          {/* Avatar */}
-          <div className="relative flex justify-center pt-8">
-            <div className="relative">
-              {/* Glow phía sau */}
-              <div className="absolute inset-0 rounded-full bg-orange-200/40 blur-xl" />
+      <div className="px-4 py-10 bg-white">
+        {/* Header */}
+        <div className="w-full max-w-md mx-auto">
+          {/* Card */}
+          <div className=" overflow-hidden">
+            {/* Avatar */}
+            <div className="relative flex justify-center pt-8">
+              <div className="relative">
+                {/* Glow phía sau */}
+                <div className="absolute inset-0 rounded-full bg-orange-200/40 blur-xl" />
 
-              {/* Avatar */}
-              <div className="relative w-28 h-28 rounded-full bg-white p-1 shadow-lg">
-                <img
-                  src="https://i.pravatar.cc/300"
-                  alt="Avatar"
-                  className="w-full h-full rounded-full object-cover"
-                />
+                {/* Avatar */}
+                <div className="relative w-28 h-28 rounded-full bg-white p-1 shadow-lg">
+                  <img
+                    src="https://i.pravatar.cc/300"
+                    alt="Avatar"
+                    className="w-full h-full rounded-full object-cover"
+                  />
+                </div>
               </div>
             </div>
-          </div>
 
-          {/* Info */}
-          <div className="px-6 py-8 space-y-6">
-            <InfoRow label="Họ và tên" value={profile.name} />
-            <InfoRow label="Số điện thoại" value={profile.phone} />
+            {/* Info */}
+            <div className="px-6 py-8 space-y-6">
+              <InfoRow label="Họ và tên" value={profileData?.fullName || "Chưa có"} />
+              <InfoRow label="Số điện thoại" value={profileData?.phone || "Chưa có"} />
 
-            {/* Button */}
-            <div className="pt-4">
-              <button
-                onClick={() => setIsEditing(true)}
-                className="w-full flex items-center justify-center gap-2
+              {/* Button */}
+              <div className="pt-4">
+                <button
+                  onClick={() => setIsEditing(true)}
+                  className="w-full flex items-center justify-center gap-2
                   bg-red-600 hover:bg-red-700
                   text-white font-semibold
                   py-3 rounded-xl
                   shadow-md hover:shadow-lg
                   transition-all active:scale-95 cursor-pointer"
-              >
-                <Pencil size={18} />
-                Chỉnh sửa thông tin
-              </button>
+                >
+                  <Pencil size={18} />
+                  Chỉnh sửa thông tin
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-   </div>
   );
 }
 
