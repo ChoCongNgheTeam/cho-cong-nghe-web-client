92da10835dae454f1a99ee36da7f76333460cf1b
trong1122002 <lht1122002@gmail.com>
2026-03-24 20:24:15 +0700

    fix:fixsauwr lại giao diện footer sửa blog responsive

--- app/(client)/blog/layout.tsx
+++ app/(client)/blog/layout.tsx
@@ -1,6 +1,6 @@
 ﻿export default function BlogLayout({ children }: { children: React.ReactNode }) {
   return (
-    <div className=\"container mt-0 mb-10 mx-auto px-4 pt-4 pb-8 sm:px-6 lg:px-0\">
+    <div className="container mt-0 mb-10 mx-auto px-4 pt-4 pb-8 sm:px-6 lg:px-0">
       {children}
     </div>
   );
