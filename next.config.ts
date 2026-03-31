6e1adbf2ccc38afa32cf2e825a5a3fadd34fd184
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2026-03-31 12:01:36 +0700

    fix err

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-   images: {
-      domains: [
-         "store.storeimages.cdn-apple.com",
-         "images.unsplash.com",
-         "cdn2.fptshop.com.vn",
-         "cdn2.cellphones.com.vn",
-         "res.cloudinary.com",
-         "lh3.googleusercontent.com",
-         "graph.facebook.com",
-         "platform-lookaside.fbsbx.com",
-      ],
-      remotePatterns: [
-         {
-            protocol: "https",
-            hostname: "**.fbcdn.net",
-         },
-      ],
-   },
-   productionBrowserSourceMaps: false,
+  images: {
+    domains: [
+      "store.storeimages.cdn-apple.com",
+      "images.unsplash.com",
+      "cdn2.fptshop.com.vn",
+      "cdn2.cellphones.com.vn",
+      "res.cloudinary.com",
+      "lh3.googleusercontent.com",
+      "graph.facebook.com",
+      "platform-lookaside.fbsbx.com",
+    ],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**.fbcdn.net",
+      },
+    ],
+  },
+  typedRoutes: false,
+  productionBrowserSourceMaps: false,
 };
 
 export default nextConfig;
