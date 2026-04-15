7d8592f0559e3d149e2553d65d175b7972d05cb0
Nkduy23 <nguyenkhanhduy23803@gmail.com>
2026-04-15 09:41:25 +0700

    remove unoptimized

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,45 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    unoptimized: true,
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "store.storeimages.cdn-apple.com",
+      },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn2.fptshop.com.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "cdn2.cellphones.com.vn",
+      },
+      {
+        protocol: "https",
+        hostname: "res.cloudinary.com",
+      },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "graph.facebook.com",
+      },
+      {
+        protocol: "https",
+        hostname: "platform-lookaside.fbsbx.com",
+      },
+      {
+        protocol: "https",
+        hostname: "**.fbcdn.net",
+      },
+    ],
+    qualities: [75, 80],
   },
 
   typedRoutes: false,
